9c04d3264081d2485db7b57aa76d1707054f7564
Nahom <nahomohani@gmail.com>
2023-07-29 10:00:28 +0300

    {% url app_name:name[path] %} is done and edit page open and working on

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from . import views
 
+app_name = "wiki"
+
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/", views.search, name="seeee"),
+    path("wiki/", views.index, name="wiki"),
     path("wiki/<str:name>", views.find, name="find"),
-    path("search/", views.search, name="search")
+    path("search/", views.search, name="search"),
+    path("add/", views.add, name="add"),
+    path("random/", views.randomm, name="random"),
+    path("edit/<str:file>", views.edit, name="edit"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,11 +1,21 @@
 from django import forms
 from django.http import HttpResponse
+
 from django.shortcuts import render
+
 from markdown2 import Markdown
 from . import util
+import random
 
 markit = Markdown()
-
+class CreateNewWiki(forms.Form):
+    title = forms.CharField(label="Title", max_length=100, 
+                    widget=forms.TextInput(attrs={'placeholder': 'Search'}))
+    # content = forms.Textarea(label="Content", attrs={'type': 'markdwo'}) 
+    content = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Content in markdown format \
+                                                           ## Django \
+                                                           # Django is python FrameWork for backend development. '}))
+    
 
 
 def index(request):
@@ -14,11 +24,7 @@ def index(request):
         })
 
 
-        
-    # return HttpResponse(f"Hello, {to} Done!")
-
 def find(request, name):
-
     title = util.get_entry(name)
     if title:
         return render(request, "encyclopedia/title.html", {
@@ -27,39 +33,73 @@ def find(request, name):
         })
     else:
         return render(request, "encyclopedia/error.html")
-    # return HttpResponse(f"Hello, {name.capitalize()}!")
+
 
 def search(request):
     if request.method == "POST":
         title = request.POST.getlist("q")[0]
-
         topic = util.get_entry(title)
+
         if topic:
-            return render(request, "encylopedia/title.html", {
+            return render(request, "encyclopedia/title.html", {
                 "title": title.capitalize(),
                 "body": markit.convert(topic),
-            }
-            )
-
+            })
         else:
             # Get all the entries
             all_pages = util.list_entries()
-            
             # Similar pages
             similar_pages = []
-
             # search if there is similarity b/n them 
             for page in util.list_entries():
                 if title.lower() in page.lower():
                     similar_pages.append(page)
-                
-
             # display all of them
             return render(request, 'encyclopedia/search.html', {
                 "search": similar_pages,
             })
-
-            # return HttpResponse("Working on it!")
-        
     else:
         return index(request)
+
+def add(request):
+    if request.method == "POST":
+        form = CreateNewWiki(request.POST)
+
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            if util.get_entry(title):
+                return HttpResponse("There is another title by this name.")
+            content = form.cleaned_data["content"]
+        print(content)
+        # util.save_entry(title, content)
+        return HttpResponse("Working on it")
+    
+    else:
+        form = CreateNewWiki()
+
+        return render(request, 'encyclopedia/addpage.html', {
+            "form": form,
+        })
+
+def randomm(request):
+    topics = util.list_entries()
+    l = random.randrange(0, len(topics))
+    title = util.get_entry(topics[l])
+    return render(request, 'encyclopedia/title.html', {
+        "title":  topics[l],
+        "body": markit.convert(title)
+    })
+
+
+def edit(request, file):
+    if request.method == "POST":
+        topic = CreateNewWiki(initial={'title': file, 'content': util.get_entry(file)})
+
+            # editForm = editPage(initial={'title': name, 'textContent': util.get_entry(name)})
+
+    # topic = util.get_entry(file)
+    # editfile = CreateNewWiki(topic)
+        return render(request, 'encyclopedia/edit.html', {
+            "edit": topic,
+        })
+    return HttpResponse("Hello, World")
